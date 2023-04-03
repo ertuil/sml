2ed4ce06f6d936f409b49719294eb036f4886361
elliot <elliot.98@outlook.com>
2023-04-03 22:22:14 +0800

    [docs] add readme and

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # SML (server monitor light)
 
-TBD
+A simple single-node server performance monitor with notification.
+
+## Usage
+
+Clone this repo:
+
+``` bash
+git clone https://github.com/ertuil/sml.git
+```
+
+Install dependence:
+
+```
+pip install -r requirements
+apt install smartmontools  # S.M.A.R.T information (For Debian or Ubuntu)
+```
+
+copy `config.example.py` to `config.py`, and change the configurations as needed.
+
+Run the single shot with
+
+``` bash
+python3 main.py
+```
+
+Or, change the configure to `interval = 300`. The `sml` works like a daemon and collects information with every 300 seconds.
+
+## Copy and License
+
+see `LICENSE`
--- config.example.py
+++ config.example.py
@@ -1,52 +1,49 @@
 # ===== global settings =====
-debug = True
-host = "localhost"
-interval = 0  # in seconds
+debug = True                    # enable debug mode
+host = "localhost"              # the node's name
+interval = 0                    # collect information periodically (in seconds)
 
 # ===== monitor settings =====
-cpu_load_warn = 0.7
-cpu_load_critical = 0.95
-cpu_interval = 1  # measure in ? second(s)
+cpu_load_warn = 0.7             # warning CPU load, >= 70%
+cpu_load_critical = 0.95        # critical CPU load, >= 95%
+cpu_interval = 1                # measure CPU load within 1 second(s)
 
-mem_load_warn = 0.7
-mem_load_critical = 0.95
+mem_load_warn = 0.7             # warning memory usage, >= 70%
+mem_load_critical = 0.95        # critical memory usage, >= 95%
 
-disk_load_warn = 0.7
-disk_load_critical = 0.9
-disk_filter = None
-# disk_filter = ["/dev/sdc"]
+disk_load_warn = 0.7            # warning disk usage, >= 70%
+disk_load_critical = 0.9        # critical disk usage, >= 90%
+disk_filter = None              # collect information on selected disks, e.g., disk_filter = ["/dev/sdc"]
 
-net_send_warn = 200  # in Mbps
-net_recv_warn = 200  # in Mbps
-net_bps_critical = 800  # in Mbps
-net_send_packet_warn = 5000  # in pps
-net_recv_packet_warn = 5000  # in pps
-net_pps_critical = 7000  # in pps
-# net_filter = None
-net_filter = ["lo", "eth0"]
-net_interval = 1  # measure in ? second(s)
+net_send_warn = 200             # warning network send rate, >= 200 Mbps
+net_recv_warn = 200             # warning network recv rate, >= 200 Mbps
+net_bps_critical = 800          # critical network rate, >= 800 Mbps
+net_send_packet_warn = 5000     # warning network send rate, >= 5000 pps
+net_recv_packet_warn = 5000     # warning network recv rate, >= 5000 pps
+net_pps_critical = 7000         # critical network send rate, >= 7000 pps
+net_filter = None               # collect information only on selected nic(s), e.g., net_filter = ["lo", "eth0"]
+net_interval = 1                # collect network information within 1 second(s)
 
-temp_cpu_warn = 70
-temp_disk_warn = 40
-temp_warn_percent = 0.7
-temp_cpu_critical = 80
-temp_disk_critical = 50
-temp_critical_percent = 0.8
+temp_cpu_warn = 70              # warning CPU temperature, >= 70 Celsius
+temp_disk_warn = 40             # warning disk temperature, >= 40 Celsius
+temp_warn_percent = 0.7         # warning misc temperature, >= 70% critical temperature (if exists)
+temp_cpu_critical = 80          # critical CPU temperature, >= 80 Celsius
+temp_disk_critical = 50         # warning disk temperature, >= 50 Celsius
+temp_critical_percent = 0.8     # warning misc temperature, >= 80% critical temperature (if exists)
 
 
-listen_map = []
-# listen_map = [("dns", "udp", 53)]
+listen_map = []                 # watch listen TCP/UDP sockets, e.g., listen_map = [("dns", "udp", 53)]
 
 # ===== notify settings =====
-log_file = 'sml.log'
-log_interval = 7  # in days
-log_reserve = 26  # about 6 months
+log_file = 'sml.log'            # log notifier, enabled if the filename is specified
+log_interval = 7                # enable log rotating, single log file for 7 days
+log_reserve = 26                # maximized rotating log files
 
-mail_from = ""
-mail_password = ""
-mail_smtp = ""
-mail_tls = True
-mail_to = [""]
+mail_from = ""                  # mail notifier, enabled if mail_from is specified
+mail_password = ""              # SMTP authentication password
+mail_smtp = ""                  # SMTP server domain
+mail_tls = True                 # SMTP with TLS (default port 465)
+mail_to = [""]                  # SMTP receiver list
 
-tg_server = ""
-tg_secret = ""
+tg_server = ""                  # tg bot notifier, enabled if bot relay server is specified
+tg_secret = ""                  # tg bot secret, see https://gist.github.com/ertuil/6105c5888fffcb6bc548ffbb2a0560e5
